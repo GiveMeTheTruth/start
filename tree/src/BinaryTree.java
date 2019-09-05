00926414f746fc107d2f7c50bbbc2321eab157f7
noone <947344751@qq.com>
2019-09-05 16:03:09 +0800

    fix

--- tree/src/BinaryTree.java
+++ tree/src/BinaryTree.java
@@ -17,11 +17,19 @@ public class BinaryTree {
         this.mRoot = root;
     }
 
+    /**
+     * 插入左子节点
+     * @param child 节点
+     */
     public void insertLeftChild(BinaryTreeNode child) {
         checkTreeEmpty();
         mRoot.setLeftNode(child);
     }
 
+    /**
+     * 插入右子节点
+     * @param child 节点
+     */
     public void insertRightChild(BinaryTreeNode child) {
         checkTreeEmpty();
         mRoot.setRightNode(child);
@@ -88,6 +96,11 @@ public class BinaryTree {
         return leftSize + rightSize + 1;
     }
 
+    /**
+     * 获取指定节点的父节点
+     * @param node 节点
+     * @return 父节点
+     */
     public BinaryTreeNode getParent(BinaryTreeNode node) {
 
         if(node == mRoot || node == null) {
@@ -121,5 +134,89 @@ public class BinaryTree {
             return getParent(subNode.getRightNode(), node);
         }
     }
+
+    // 遍历
+    // 根据不同的场景中，根节点、左右子树遍历的顺序，二叉树的遍历分为三种
+    // 1、先序遍历
+    // 2、中序遍历
+    // 3、后序遍历
+    // 这里先序、中序、后序指的是 根节点相对左右子树的遍历顺序。
+
+    // 先序遍历
+    // 即根节点在左右子树之前遍历：
+    // 先访问根节点
+    // 再先序遍历左子树
+    // 再先序遍历右子树
+    // 退出
+
+    /**
+     * 先序遍历
+     * @param node 节点
+     */
+    public void iterateFirstOrder(BinaryTreeNode node) {
+
+        if(node == null) {
+            return;
+        }
+
+        operate(node);
+        iterateFirstOrder(node.getLeftNode());
+        iterateFirstOrder(node.getRightNode());
+    }
+
+    /**
+     * 模拟操作
+     * @param node 节点
+     */
+    private void operate(BinaryTreeNode node) {
+        if(node == null) {
+            return;
+        }
+
+        System.out.println(node.getData());
+    }
+
+    // 中序遍历
+    // 遍历顺序：
+    // 先中序遍历左子树
+    // 再访问根节点
+    // 再中序遍历右子树
+    // 退出
+
+    /**
+     * 中序遍历
+     * @param node 节点
+     */
+    public void iterateMediumOrder(BinaryTreeNode node) {
+        if(node == null) {
+            return;
+        }
+        iterateMediumOrder(node.getLeftNode());
+        operate(node);
+        iterateMediumOrder(node.getRightNode());
+
+    }
+
+    // 后序遍历
+    // 即根节点在左右子树之后遍历：
+    // 先后序遍历左子树
+    // 再后序遍历右子树
+    // 最后访问根节点
+    // 退出
+
+    /**
+     * 后序遍历
+     * @param node 节点
+     */
+    public void iterateLastOrder(BinaryTreeNode node) {
+        if(node == null) {
+            return;
+        }
+
+        iterateLastOrder(node.getLeftNode());
+        iterateLastOrder(node.getRightNode());
+        operate(node);
+    }
+
 
 }
